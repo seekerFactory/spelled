38760930e0bbb5a683bd787c1daee2d34d8b049b
seekerFactory <shadow.seeker.me@gmail.com>
2014-08-18 01:02:54 +0530

    small update

--- test2.py
+++ test2.py
@@ -16,10 +16,12 @@ from tools.ngram import *
 
 def spelltest(tests, bias=None, verbose=True):
     import time
+
     n, bad, unknown, start = 0, 0, 0, time.clock()
     if bias:
         for target in tests:
             NWORDS[target] += bias
+
     for target,wrongs in tests.items():
         for wrong in wrongs.split():
             n += 1
@@ -29,23 +31,24 @@ def spelltest(tests, bias=None, verbose=True):
                 unknown += (target not in NWORDS);
                 if verbose:
                     print('correct(%r) => %r (%d); expected %r (%d)' % (wrong, w, NWORDS[w], target, NWORDS[target]))
-    
+
     return dict(bad=bad, n=n, bias=bias, pct=int(100. - 100.*bad/n), unknown=unknown, secs=int(time.clock()-start))
 
 #if __name__ == '__main__':
 def main():
+    #    global _lang, NWORDS;
+    
 ######### Change these as needed #############
-	lang = "english"
-	bias, verbose, timesrun = None, True, 2
+    lang = "english"
+    bias, verbose, timesrun = None, True, 2
 ##############################################
-	
-	dataset=[];
-	_lang, NWORDS = setGlobalsWithLanguage(lang);
+    dataset=[];
+    _lang, NWORDS = setGlobalsWithLanguage(lang);
 
-	for i in range(timesrun):
-		dataset.append(("Ran "+str(i+1), spelltest(test2.test(), bias, verbose)));
-	
-	for item in dataset: print(item)
+    for i in range(timesrun):
+        dataset.append(("Ran "+str(i+1), spelltest(test2.test(), bias, verbose)));
+
+    for item in dataset: print(item)
 
 if __name__ == '__main__':
 	main()
